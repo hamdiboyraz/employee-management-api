e4eec84332ede4b62be49be1854662eca3a5573d
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-07-28 06:34:11 +0300

    Complete project

--- src/main/java/com/example/employeemanagementapi/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagementapi/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.example.employeemanagementapi.controller;
 import com.example.employeemanagementapi.model.Employee;
 import com.example.employeemanagementapi.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*") // http://localhost:3000
 @RestController
@@ -27,5 +30,45 @@ public class EmployeeController {
     public List<Employee> getAllEmployees() {
         return employeeService.getAllEmployees();
     }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Long id) {
+        boolean deleted = false;
+        deleted = employeeService.deleteEmployee(id);
+        Map<String,Boolean> response = new HashMap<>(); //key-value
+        System.out.println(response);
+        response.put("deleted", deleted);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        if (id == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Employee employee = employeeService.getEmployeeById(id);
+        if (employee == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,
+                                                   @RequestBody Employee employee) {
+        if (id == null || employee == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Employee updatedEmployee = employeeService.updateEmployee(id, employee);
+        if (updatedEmployee == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
 
 }
